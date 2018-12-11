756959de1e0e18776cb578a891bf24a5b076a601
Daniel T. Plop <daniel.theodor.plop@gmail.com>
2018-12-11 01:59:56 +0100

    update randargmax with docstring and clean bug
    Tie break should occur only if there is more than one maximum.

--- lib/utils.py
+++ lib/utils.py
@@ -1,12 +1,18 @@
 import numpy as np
 
 def randargmax(x):
-    """
-        Argmax operator that breaks ties uniformly at random
+    """Argmax operator that breaks ties uniformly at random.
+
+    Args:
+        x (ndarray): Input array with ndim=1.
+
+    Returns:
+        int: Index at which x is maximum. If there are multiple maxima, then one of the indices is chosen uniformly at random.
+
     """
     mxs = np.amax(x)
     idxs = np.where(x == mxs)[0]
-    if len(idxs) > 0:
+    if len(idxs) > 1:
         return np.random.choice(idxs)
     else:
         return np.argmax(x)
